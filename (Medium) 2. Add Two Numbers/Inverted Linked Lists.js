175149f130e99a5eadd506d8975211b57e95308e
Luís Ulian <luissteam44@gmail.com>
2026-05-29 21:29:07 -0300

    começo da atividade

---
+++ (Medium) 2. Add Two Numbers/Inverted Linked Lists.js
@@ -0,0 +1,14 @@
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+var addTwoNumbers = function(l1, l2) {
+    
+};
+
+{ListNode} l1 = [2,4,3];
+{ListNode} l2 = [5,6,4];
+
+number = l1[0].val;
+console.log(number);
